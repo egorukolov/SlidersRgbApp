1cb2ae68364e2d14d23d9b5c4127a9bbb2a9145a
Egor Ukolov <ukolovegor@yandex.ru>
2022-11-20 19:47:59 +0700

    Initial Commit

---
+++ SlidersRgbApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlidersRgbApp
+//
+//  Created by Egor Ukolov on 20.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
